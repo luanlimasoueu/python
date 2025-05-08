730b4c19fa924731d4ebe5e1902cd582f0e28604
okluan <luanemerson8@gmail.com>
2025-05-07 21:32:12 -0300

    Study

---
+++ exercicios_livros/Python para Desenvolvedores/3_exemplo.py
@@ -0,0 +1,36 @@
+class Classe(supcl1, supcl2):
+"""
+Isto é uma classe
+"""
+clsvar = []
+def __init__(self, args):
+"""
+Inicializador da classe
+"""
+<bloco de código>
+def __done__(self):
+"""
+Destrutor da classe
+"""
+<bloco de código>
+def metodo(self, params):
+"""
+Método de objeto
+"""
+<bloco de código>
+@classmethod
+def cls_metodo(cls, params):
+"""
+Método de classe
+"""
+<bloco de código>
+@staticmethod
+def est_metodo(params):
+"""
+Método estático
+"""
+<bloco de código>
+obj = Classe()
+obj.metodo()
+Classe.cls_metodo()
+Classe.est_metodo()
