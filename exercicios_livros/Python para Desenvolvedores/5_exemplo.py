3cdd1e78201af9dfb2efc2b6f0b6d51b82fadc2d
okluan <luanemerson8@gmail.com>
2025-05-10 00:21:27 -0300

    Study

---
+++ exercicios_livros/Python para Desenvolvedores/5_exemplo.py
@@ -0,0 +1,17 @@
+class Calc:
+    def __init__(self, formula, **vars):
+        self.formula = formula
+        self.vars = vars
+        self.__recalc()
+    def __recalc(self):
+        self.__res = eval(self.formula, self.vars)
+    def __repr__(self):
+        self.__recalc()
+        return str(self.__res)
+formula = '2*x + 3*y + z**2'
+calc = Calc(formula, x=2, y=3, z=1)
+
+print ('fórmula:', calc.formula)
+#print ('x =', calc.vars['x'],'-> calc =',calc  calc.vars['x'] = 4)
+print ('x =', calc.vars['x'],'-> calc =', calc)
+print (dir(calc))
