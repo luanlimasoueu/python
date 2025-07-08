405fc85e286a0dc7ca4e9a23e8df1ff1e5c8782f
okluan <luanemerson8@gmail.com>
2025-07-08 08:17:34 -0300

    Study

--- class/7_otavio_miranda/41_aula/classes.py
+++ class/7_otavio_miranda/41_aula/classes.py
@@ -1,11 +1,19 @@
 class Escritor :
     def __init__( self, nome):
         self.__nome = nome
-        self.__ferramenta
+        self.__ferramenta = None
 
     @property
     def nome(self):
         return self.__nome
+    
+    @property
+    def ferramenta(self):
+        return self.__ferramenta
+    
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self.__ferramenta = ferramenta
 
 class Caneta:
 
@@ -16,5 +24,10 @@ class Caneta:
     def marca (self):
         return self.__marca
     
+    def escrever ( self):
+        print('Caneta está escrevendo ...')
+    
 class MaquinaDeEscrever:
-    pass
+    
+    def escrever(self):
+        print('Máquina está escrevendo .....')
--- class/7_otavio_miranda/41_aula/main.py
+++ class/7_otavio_miranda/41_aula/main.py
@@ -1,6 +1,17 @@
 from classes import Escritor
 from classes import Caneta
+from classes import MaquinaDeEscrever
 
 escritor = Escritor('Joãozinho')
 caneta = Caneta('Bic')
+maquina = MaquinaDeEscrever()
+print( escritor.nome)
 print(caneta.marca)
+maquina.escrever()
+#print(caneta.marca)
+#print(caneta.escrever())
+
+print()
+
+escritor.ferramenta = maquina
+escritor.ferramenta.escrever()
