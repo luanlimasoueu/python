1a94ef8c45f33c65874e146e5ed39079f8b50b80
okluan <luanemerson8@gmail.com>
2025-07-08 07:57:39 -0300

    Study

--- class/7_otavio_miranda/41_aula/classes.py
+++ class/7_otavio_miranda/41_aula/classes.py
@@ -1,2 +1,20 @@
 class Escritor :
     def __init__( self, nome):
+        self.__nome = nome
+        self.__ferramenta
+
+    @property
+    def nome(self):
+        return self.__nome
+
+class Caneta:
+
+    def __init__( self, marca):
+        self.__marca = marca
+
+    @property
+    def marca (self):
+        return self.__marca
+    
+class MaquinaDeEscrever:
+    pass
---
+++ class/7_otavio_miranda/41_aula/main.py
@@ -0,0 +1,6 @@
+from classes import Escritor
+from classes import Caneta
+
+escritor = Escritor('Joãozinho')
+caneta = Caneta('Bic')
+print(caneta.marca)
