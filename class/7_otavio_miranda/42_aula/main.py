8705885731b519d98a1cf221e2cf99db7f7228b3
okluan <luanemerson8@gmail.com>
2025-07-09 10:19:58 -0300

    Study

--- class/7_otavio_miranda/42_aula/main.py
+++ class/7_otavio_miranda/42_aula/main.py
@@ -1,4 +1,16 @@
 from  classes import CarrinhoDeCompras, Produto
 
 carrinho = CarrinhoDeCompras()
+
 p1 = Produto('Camiseta', 50)
+carrinho.inserir_produto(p1)
+
+p2 = Produto('Iphone', 10000)
+
+carrinho.inserir_produto(p2)
+
+p1 = Produto('Caneca', 15)
+
+carrinho.inserir_produto(p1)
+
+carrinho.lista_produto()
