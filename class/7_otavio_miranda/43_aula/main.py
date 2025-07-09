bb9f178f1ee7d56157718a0d126bc9b247da6410
okluan <luanemerson8@gmail.com>
2025-07-09 15:37:25 -0300

    Study

--- class/7_otavio_miranda/43_aula/main.py
+++ class/7_otavio_miranda/43_aula/main.py
@@ -5,3 +5,4 @@ cliente1.inserir_endereco('Belo Horizonte', 'MG')
 print( cliente1.nome)
 cliente1.lista_enderecos()
 print()
+del cliente1
---
+++ class/7_otavio_miranda/44_aula/main.py
@@ -0,0 +1,14 @@
+from classes import *
+
+"""
+Associação - Usa
+Agregação - Tem
+Composição -  É dono
+Herança - É
+"""
+
+c1 = Cliente('Luiz', 45)
+c1.falar()
+c1.comprar()
+
+
