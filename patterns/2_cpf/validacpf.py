22a075c0b690195b55ff7cffc0db7e5e0f01a173
okluan <luanemerson8@gmail.com>
2025-09-03 21:32:03 -0300

    Study

--- patterns/2_cpf/validacpf.py
+++ patterns/2_cpf/validacpf.py
@@ -1,6 +1,12 @@
 class ValidaCpf:
     def __init__ (self, cpf):
         self.cpf = cpf
+
     @property
     def cpf(self):
-        return self.cpf
+        return self._cpf
+    
+    @cpf.setter
+    def cpf( self, cpf):
+        self.cpf = cpf
+        
